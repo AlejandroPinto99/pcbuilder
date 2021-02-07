fe8904bf6d825837381721f4f214a3ec0827b38a
AlejandroPinto99 <00156618@uca.edu.sv>
2021-02-06 22:32:45 -0600

    primera sesion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+//Functional components (esto es lo mas ocupados) vs Class Components 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  //JSX -> Syntaxys that combines js and html 
+  return(
+      <div>
+        <h1 className="bg-gradient-to-r from-red-300 to-blue-500" >Hola mundo!!</h1>
+      </div>
+  )
 }
 
 export default App;
